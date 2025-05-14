4855d028acfb77e3340764fe51a69ca48a604f1f
awangran <ashleero.yin@gmail.com>
2025-05-13 21:45:46 -0500

    setup listo

---
+++ DataStructures/Graph/digraph.py
@@ -0,0 +1,48 @@
+from DataStructures.Map import map_linear_probing as map
+
+
+def new_graph(order):
+    pass
+
+def insert_vertex(my_graph, key_u, info_u):
+    pass
+
+def update_vertex_info(my_graph, key_u, new_info_u):
+    pass
+
+def remove_vertex(my_graph, key_u):
+    pass
+
+def add_edge(my_graph, key_u, key_v, weight=1.0):
+    pass
+
+def order(my_graph):
+    pass
+
+def size(my_graph):
+    pass
+
+def vertices(my_graph):
+    pass
+
+def degree(my_graph, key_u):
+    pass
+
+def get_edge(my_graph, key_u, key_v):
+    pass
+
+def get_vertex_information(my_graph, key_u):
+    pass
+
+def contains_vertex(my_graph, key_u):
+    pass
+
+def adjacents(my_graph, key_u):
+    pass
+
+def edges_vertex(my_graph, key_u):
+    pass
+
+def get_vertex(my_graph, key_u):
+    pass
+
